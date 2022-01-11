c01e7b8060b9f3fd24e90aca017cf202fad6eb96
massaaki <eu@massaaki.com>
2022-01-11 14:43:50 -0300

    chore: configure @ path default to typescript, jest and build

--- jest.config.ts
+++ jest.config.ts
@@ -1,31 +1,13 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     "<rootDir>/src/**/*.ts",
     "!<rootDir>/src/domain/**/*.ts",
   ],
 
-  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
-
-  // A list of paths to directories that Jest should use to search for files in
   roots: ["<rootDir>/src"],
-
-  // The test environment that will be used for testing
   testEnvironment: "node",
-
-  // A map from regular expressions to paths to transformers
   transform: {
     ".+\\.ts$": "ts-jest",
   },
